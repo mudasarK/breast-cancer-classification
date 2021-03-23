3898a53b99db68a7d60328cc86b5c070ac728f6c
mudasarK <mudasar.kalwar@gmail.com>
2021-03-23 09:28:24 +0900

    build dataset
    build dataset

---
+++ config/config.py
@@ -0,0 +1,11 @@
+import os
+
+INPUT_DATASET = "../breast_cancer_data"
+
+BASE_PATH = "../idc"
+TRAIN_PATH = os.path.sep.join([BASE_PATH, "training"])
+VAL_PATH = os.path.sep.join([BASE_PATH, "validation"])
+TEST_PATH = os.path.sep.join([BASE_PATH, "testing"])
+
+TRAIN_SPLIT = 0.8
+VAL_SPLIT = 0.1
